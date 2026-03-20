708af9109d28025f03126020483bac80ad9cd477
YogyashriPatil <yogyashripatil20@gmail.com>
2026-03-20 21:36:23 +0530

    Created digital clock

---
+++ digital-clock/script.js
@@ -0,0 +1,6 @@
+const div=document.querySelector('div')
+setInterval(() => {
+    let time = new Date()
+    // console.log(time.toLocaleTimeString());
+    div.textContent=time.toLocaleTimeString()
+},1000)
